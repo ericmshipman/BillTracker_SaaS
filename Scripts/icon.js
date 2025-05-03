b0248a5d250a39871f5a91596cc7d9dcc982a67e
Eric Shipman <69483551+ericmshipman@users.noreply.github.com>
2025-05-03 11:25:08 -0400

    Attempting to fix logo

--- Scripts/icon.js
+++ Scripts/icon.js
@@ -1,5 +1,5 @@
 (async function() {
-    const resp = await fetch('/Content/icons.svg');
+    const resp = await fetch('Content/icons.svg');
     const text = await resp.text();
     const wrap = document.createElement('div');
     wrap.style.display = 'none';
